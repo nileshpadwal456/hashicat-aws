62ee6769b577b4e53bdb65203df923f5e83a203b
Nilesh Padwal <nileshpadwal456@gmail.com>
2020-01-09 23:46:25 +0000

    Added VPC module

--- vpc.tf
+++ vpc.tf
@@ -1,4 +1,15 @@
+//--------------------------------------------------------------------
+// Modules
 module "vpc" {
   source  = "app.terraform.io/ADVALENT/vpc/aws"
   version = "2.21.0"
+
+  database_subnet_assign_ipv6_address_on_creation = "false"
+  elasticache_subnet_assign_ipv6_address_on_creation = "false"
+  enable_classiclink = "false"
+  enable_classiclink_dns_support = "false"
+  intra_subnet_assign_ipv6_address_on_creation = "false"
+  private_subnet_assign_ipv6_address_on_creation = "false"
+  public_subnet_assign_ipv6_address_on_creation = "false"
+  redshift_subnet_assign_ipv6_address_on_creation = "false"
 }
